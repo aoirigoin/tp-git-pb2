5a15dc7777db3c03f6753446e3c3bf391d65cf9c
alan <airigoin@alumno.unlam.edu.ar>
2020-04-20 20:34:05 -0200

    cambio de los 2 primeros metodos

--- src/git/Calculadora.java
+++ src/git/Calculadora.java
@@ -9,33 +9,38 @@ public class Calculadora {
 	 Integer opcion;
 	 Integer resultado;
 	 
+	 
 	 do{
 	 System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
 	 opcion = teclado.next();
 	 }
-	 while(opcion < 1 && opcion > 4)
+	 while(opcion < 1 && opcion > 4);
 	 System.out.println("Ingrese el primer numero");
 	 a = teclado.next();
 	 System.out.println("Ingrese el segundo numero");
 	 a = teclado.next();
 	 
-	 switch (opcion) {
-		case 1:
-			System.out.println("Elegiste la opcion suma");
-			resultado = a + b;
-			break;
-		case 2:
+	 public Integer metodoQueMultiplicaDosNumeros(Integer a, Integer b) {
+		  Integer resultado = a * b;
+		 System.out.println("El resultado es"+resultado);
+		 return	resultado ;
+	 }
+		
+	 public Integer metodoQueDivideDosNumeros(Integer a, Integer b) {
+		 
+		    Integer resultado = a / b;
+			 System.out.println("El resultado es"+resultado);
+			 return	resultado ;
+		
+	 }	
+		
+		case 3:
 			System.out.println("Elegiste la opcion resta");
 			resultado = a - b;
 			break;
-
-		case 3:
-			System.out.println("Elegiste la opcion multipacion");
-			resultado = a * b;
-			break;
 		case 4:
-			System.out.println("Elegiste la opcion division");
-			resultado = a / b;
+			System.out.println("Elegiste la opcion suma");
+			resultado = a + b;
 			break;
 		}
 	 System.out.println("El resultado es " + resultado.toString());
