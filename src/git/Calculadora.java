f6e752439239a85975efa3406abe78568bc940a3
mariano <mariano_ariel_97@outlook.com>
2020-04-20 20:51:18 -0300

    Cambio a metodos suma y resta

--- src/git/Calculadora.java
+++ src/git/Calculadora.java
@@ -1,39 +1,54 @@
 package git;
-
+import java.util.Scanner;
 public class Calculadora {
-
+	
 	
 	public static void main(String[] args) {
 	 Integer a;
 	 Integer b;
 	 Integer opcion;
 	 Integer resultado;
+	 Scanner teclado = new Scanner(System.in);
 	 
 	 
 	 do{
 	 System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
-	 opcion = teclado.next();
+	 opcion = teclado.nextInt();
 	 }
 	 while(opcion < 1 && opcion > 4);
 	 System.out.println("Ingrese el primer numero");
-	 a = teclado.next();
+	 a = teclado.nextInt();
 	 System.out.println("Ingrese el segundo numero");
-	 a = teclado.next();
+	 a = teclado.nextInt();
 	 
+	 //System.out.println("El resultado es " + a );
+	}
 
 	public Integer metodoQueMultiplicaDosNumeros(Integer a, Integer b) {
 		  Integer resultado = a * b;
-		  System.out.println("El resultado es " + resultado.toString());
+		 
 		 return	resultado ;
 	 }
 		
-	 Integer metodoQueDivideDosNumeros(Integer a, Integer b) {
+	 public Integer metodoQueDivideDosNumeros(Integer a, Integer b) {
 		 
-		    Integer resultado1 = a / b;
-		    System.out.println("El resultado es " + resultado1.toString());
-			 return resultado1;
+		    Integer resultado = a / b;
+		    
+			 return resultado;
 		
 	 }	
+	 public Integer metodoQueSumaDosNumeros(Integer a, Integer b) {
+		  Integer resultado = a + b;
+		  
+		 return	resultado ;
+	 }
+	 public Integer metodoQueRestaDosNumeros(Integer a, Integer b) {
+		  Integer resultado = a - b;
+		  
+		 return	resultado ;
+	 }
+	 
+	 
 		
 	
 	 
